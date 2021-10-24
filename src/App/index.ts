1c76d42604516f9625e2dacc022ab0c10a101849
你好2007 <2501482523@qq.com>
2021-10-25 00:01:14 +0800

    优化一些参数

--- src/App/index.ts
+++ src/App/index.ts
@@ -27,12 +27,12 @@ export default class {
 
         let painter = image3d.Painter() // 画笔
         let buffer = image3d.Buffer() // 缓冲区
-        let cammera = image3d.Camera({ size: 250 }) // 相机
+        let cammera = image3d.Camera({ size: [450, 250, 250] }) // 相机
 
         setInterval(() => {
 
             // 首先，每次围绕x轴旋转一点点
-            image3d.setUniformMatrix("u_camera", cammera.rotateBody(0.02, -1, 0, 0, 1, 0, 0).value())
+            image3d.setUniformMatrix("u_camera", cammera.rotateBody(0.02, -1, 0.2, 0, 1, 0.2, 0).value())
 
             // 对于变换矩阵，只有个别需要，因此先使用单位矩阵E初始化
             image3d.setUniformMatrix("u_matrix", [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1])
@@ -40,7 +40,7 @@ export default class {
             // 然后，借助三维坐标运算库来绘制元素
 
             let threeGeometry = ThreeGeometry({
-                precision: 15
+                precision: 20
             })
 
             // 设置为绘制氧原子颜色
@@ -75,22 +75,26 @@ export default class {
             image3d.setUniformMatrix("u_matrix", image3d.Camera().rotateBody(Math.PI * 0.25, -70, 0, 0, -70, 0, -1).value())
 
             threeGeometry.prism(function (data) {
-                buffer.write(new Float32Array(data.points)).use('a_position', 3, 3, 0)
-                painter['draw' + data.methods](0, data.length)
+                if (data.methods == 'StripTriangle') {
+                    buffer.write(new Float32Array(data.points)).use('a_position', 3, 3, 0)
+                    painter.drawStripTriangle(0, data.length)
+                }
 
-            }, -70, 0, 0, 16, 100, 10)
+            }, -70, 0, 0, 16, 60, 14)
 
             //  绘制化学键（右）
 
             image3d.setUniformMatrix("u_matrix", image3d.Camera().rotateBody(Math.PI * 0.25, 70, 0, 0, 70, 0, 1).value())
 
             threeGeometry.prism(function (data) {
-                buffer.write(new Float32Array(data.points)).use('a_position', 3, 3, 0)
-                painter['draw' + data.methods](0, data.length)
+                if (data.methods == 'StripTriangle') {
+                    buffer.write(new Float32Array(data.points)).use('a_position', 3, 3, 0)
+                    painter.drawStripTriangle(0, data.length)
+                }
 
-            }, 70, 0, 0, 16, 100, 10)
+            }, 70, 0, 0, 16, 60, 14)
 
-        }, 70)
+        }, 50)
 
     }
 
